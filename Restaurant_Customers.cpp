0bd57307ac11a5b9ab426b14ae74449a16c35941
Abhimanyu Maurya <aerma7309@gmail.com>
2020-12-28 10:10:40 +0530

    Restaurant_Customers

---
+++ Restaurant_Customers.cpp
@@ -0,0 +1,33 @@
+/*
+*    @Date    : 2020-12-28 08:54:06
+*    @Author  : aerma7309 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+int FastIO = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+const int inf = 1e9 + 5;
+signed main()
+{
+    int n, cnt = 0, ans = -1e9 + 5;
+    cin >> n;
+    vector<int> a(n), d(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i] >> d[i];
+    sort(a.begin(), a.end());
+    sort(d.begin(), d.end());
+    for (int i = 0, j = 0; i < n and j < n;)
+    {
+        while (j < n and d[j] <= a[i])
+            cnt--, j++;
+        while (i < n and a[i] < d[j])
+            cnt++, i++, ans = max(ans, cnt);
+    }
+    cout << ans;
+    return 0;
+}
